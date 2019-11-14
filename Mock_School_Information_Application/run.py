5ed9f878eb0ceb5029f99d080c2edd70f78ae875
John Sy <13071774+DarkHorse108@users.noreply.github.com>
2019-11-14 00:57:03 -1000

    Reorganized file directories to match functioning webapp. Generic db credential files, run.py, and requirements uploaded so webapp can be replicated with user supplied information.

---
+++ Mock_School_Information_Application/run.py
@@ -0,0 +1,7 @@
+#this file is used to run your flask-based-database-interacting-website persistently!
+
+#change this line to run the app that you want to run
+from db_connector.Jackson import app
+#for example, the above line tells to run the sample db connection app in db_connector/ directory
+
+
