e67f128b7ca0ef838b56f5f6070de84a01440f0b
John Sy <13071774+DarkHorse108@users.noreply.github.com>
2019-11-14 01:01:08 -1000

    Removed reference to example table/database from db_connector

--- Mock_School_Information_Application/db_connector/db_connector.py
+++ Mock_School_Information_Application/db_connector/db_connector.py
@@ -48,7 +48,8 @@ def execute_query(db_connection = None, query = None, query_params = ()):
 if __name__ == '__main__':
     print("Executing a sample query on the database using the credentials from db_credentials.py")
     db = connect_to_database()
-    query = "SELECT * from bsg_people;"
+    #Change TABLE NAME below for sample query.
+    query = "SELECT * from TABLE NAME;"
     results = execute_query(db, query);
     print("Printing results of %s" % query)
 
